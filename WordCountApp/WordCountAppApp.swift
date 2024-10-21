ed11917301e5586ba372a8289c3333ba867b6bef
Sourajyesh <sourajyesh.karmakar@gmail.com>
2024-10-21 22:06:30 +0530

    Initial Commit

---
+++ WordCountApp/WordCountAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCountAppApp.swift
+//  WordCountApp
+//
+//  Created by Sourajyesh on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WordCountAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordCountAppTests/WordCountAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordCountAppTests.swift
+//  WordCountAppTests
+//
+//  Created by Sourajyesh on 21/10/24.
+//
+
+import Testing
+@testable import WordCountApp
+
+struct WordCountAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
